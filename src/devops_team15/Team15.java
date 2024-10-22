533cb710efe7e8545f072aa46c1f35c85dd6fe03
NAVEEN <NAVEEN@LAPTOP-5ARVGEEI>
2024-10-22 10:43:05 +0530

    commit the data

--- src/devops_team15/Team15.java
+++ src/devops_team15/Team15.java
@@ -5,7 +5,8 @@ public class Team15 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(2*4);
-
+		System.out.println("Hello World");
+		
 	}
 
 }
