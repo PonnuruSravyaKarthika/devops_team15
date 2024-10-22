0655039e8c9ada9f38f1a32e7297fa27b7e34613
NAVEEN <NAVEEN@LAPTOP-5ARVGEEI>
2024-10-22 10:45:08 +0530

    commit

---
+++ src/devops_team15/Basic.java
@@ -0,0 +1,12 @@
+package devops_team15;
+
+public class Basic{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(2*4);
+		System.out.println("Hello World");
+		
+	}
+
+}
